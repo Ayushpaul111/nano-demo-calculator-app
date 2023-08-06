6bf4ffeacd6b6dd641732e2938b1503754473505
Ayush Paul <ayushpaul1111@gmail.com>
2023-08-06 23:37:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,31 @@
-from flask import Flask
-
+from flask import Flask, request
 app = Flask(__name__)
-
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    response_content = "Code:200\nContent:hello world!"
+    return response_content, 200, {'Content-Type':'text/plain'}
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.json
+    first = data['first']
+    second = data['second']
+    result = first + second
+    
+    response_content = f"Status code:200\nContent:{{ result: {result} }}"
+    return response_content, 200, {'Content-Type':'text/plain'}
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.json
+    print("Received data:", data) 
+    first = data['first']
+    second = data['second']
+    result = first - second
+    
+    response_content = f"Status code:200\nContent:{{ result: {result} }}"
+    return response_content, 200, {'Content-Type':'text/plain'}
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
